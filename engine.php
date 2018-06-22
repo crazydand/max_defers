b71df096a147cbfec96eb634c45b37bbf3cf4e61
Daniel Dela Torre <39947196+crazydand@users.noreply.github.com>
2018-06-22 13:41:29 +0800

    Update engine.php

--- engine.php
+++ engine.php
@@ -2,6 +2,7 @@
     require 'engine-class.php';
     $notification = "Enter the domain";
     $domain = $_POST['txt_domain'];
+    $get_total_deferd_list = "ssh dan.d@jumpoff1.per01.ds.network '/usr/bin/dsh -c -F 30 -M -g cpanel_production_au -- sudo ls -lah /var/cpanel/email_send_limits | grep max_deferfail | wc -l'";
     if(isset($_POST['domain_search']))
     {
         $obj = new Domain;
